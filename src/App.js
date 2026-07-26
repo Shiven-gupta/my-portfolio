326c45a5fe683cbe32591f58044f84b67fd611a6
Shiven-gupta <g.shivengupta.g@gmail.com>
2026-07-27 02:19:20 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -6,32 +6,42 @@ import "aos/dist/aos.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import TechStack from "./components/TechStack"; 
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import Certifications from "./components/Certification";
-import CustomCursor from "./components/CustomCursor"; 
-import ChatBot from "./components/ChatBot"; 
+import CustomCursor from "./components/CustomCursor";
+import ChatBot from "./components/ChatBot";
 
 export default function App() {
   useEffect(() => {
     AOS.init({ duration: 800, once: false });
     window.addEventListener("scroll", AOS.refresh);
+
+    return () => {
+      window.removeEventListener("scroll", AOS.refresh);
+    };
   }, []);
 
   return (
     <div className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition-colors duration-300 font-sans scroll-smooth relative">
-      <CustomCursor /> {}
+      <CustomCursor />
 
       <Header />
 
       <main className="pt-20">
+
         <section id="hero">
           <Hero />
         </section>
 
         <section id="about" className="py-12">
           <About />
+        </section>
+
+        <section id="tech-stack" className="py-12">
+          <TechStack />
         </section>
 
         <section id="skills" className="py-12">
@@ -49,8 +59,10 @@ export default function App() {
         <section id="contact" className="py-12">
           <Footer />
         </section>
+
       </main>
-      <ChatBot/>
+
+      <ChatBot />
     </div>
   );
 }
