2c173f5cba25b126748dac9b7b2a2c3b712884c4
Shiven-gupta <g.shivengupta.g@gmail.com>
2026-07-27 02:00:03 +0530

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,24 +17,14 @@ export default function Footer() {
             <FaEnvelope /> g.shivengupta.g@gmail.com
           </a>
           <a
-            href="https://github.com/Shiven-gupta"
-            target="_blank"
-            className="flex items-center gap-2 hover:text-yellow-500 transition-colors"
-          >
+            href="https://github.com/Shiven-gupta" target="_blank" className="flex items-center gap-2 hover:text-yellow-500 transition-colors" >
             <FaGithub /> github.com/Shiven-gupta
           </a>
           <a
-            href="https://www.linkedin.com/in/gshiven-guptag/"
-            target="_blank"
-            className="flex items-center gap-2 hover:text-yellow-500 transition-colors"
-          >
+            href="https://www.linkedin.com/in/gshiven-guptag/" target="_blank" className="flex items-center gap-2 hover:text-yellow-500 transition-colors" >
             <FaLinkedin /> linkedin.com/in/gshiven-guptag
           </a>
-          <a
-            href="https://www.hackerrank.com/profile/g_shivengupta_g"
-            target="_blank"
-            className="flex items-center gap-2 hover:text-yellow-500 transition-colors"
-          >
+          <a href="https://www.hackerrank.com/profile/g_shivengupta_g" target="_blank" className="flex items-center gap-2 hover:text-yellow-500 transition-colors" >
             <FaHackerrank /> hackerrank.com/g_shivengupta_g
           </a>
         </div>
